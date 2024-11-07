3c1f069e6467c300417bcfc689d734aea4cc44fa
MELTY <70095127+meltytech@users.noreply.github.com>
2024-11-08 01:12:24 +0200

    Delete header.php

--- header.php
+++
@@ -1,1 +0,0 @@
-<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>Modular Website</title> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> </head> <body>
